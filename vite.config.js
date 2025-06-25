272cc028870b9d7e744e2cc464749851669e92fd
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2025-06-25 13:57:47 +0300

    fix(paths): fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/to-do-list",
+  base: "/to-do-list/",
 })
